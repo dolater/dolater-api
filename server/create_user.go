da9f166595dcda10b50782040a1fcf6b35df3dd3
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-10 16:59:22 +0900

    add create_user

--- server/create_user.go
+++ server/create_user.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/dolater/dolater-api/db"
 	api "github.com/dolater/dolater-api/generated"
+	"github.com/dolater/dolater-api/model"
 	"github.com/dolater/dolater-api/server/utility"
 	"github.com/gin-gonic/gin"
 )
@@ -36,7 +37,32 @@ func (s *Server) CreateUser(c *gin.Context) {
 		sqldb.Close()
 	}()
 
-	user := api.User{}
+	var displayName string
+	var photoURL string
+	var ok bool
+
+	displayName, ok = token.Claims["name"].(string)
+	if !ok {
+		displayName = ""
+	}
+	photoURL, ok = token.Claims["picture"].(string)
+	if !ok {
+		photoURL = ""
+	}
+
+	user := model.User{
+		Id:          token.UID,
+		DisplayName: displayName,
+		PhotoURL:    photoURL,
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		message := err.Error()
+		c.AbortWithStatusJSON(http.StatusInternalServerError, api.Error{
+			Message: &message,
+		})
+		return
+	}
 
 	c.JSON(http.StatusOK, user)
 }
