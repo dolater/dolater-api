3680562e722184598af3b7577a4595b7ece19a00
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-11 14:55:03 +0900

    Add updateUser (#6)
    * add updateUser
    
    * fix update user
    
    * fix create user

--- server/update_user.go
+++ server/update_user.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/dolater/dolater-api/db"
 	api "github.com/dolater/dolater-api/generated"
+	"github.com/dolater/dolater-api/model"
 	"github.com/dolater/dolater-api/server/utility"
 	"github.com/gin-gonic/gin"
 )
@@ -36,7 +37,28 @@ func (s *Server) UpdateUser(c *gin.Context, id string) {
 		sqldb.Close()
 	}()
 
-	user := api.User{}
+	var requestBody api.UpdateUserInput
+	if err := c.BindJSON(&requestBody); err != nil {
+		message := err.Error()
+		c.AbortWithStatusJSON(http.StatusBadRequest, api.Error{
+			Message: &message,
+		})
+		return
+	}
+
+	user := model.User{
+		Id:          id,
+		DisplayName: requestBody.DisplayName,
+		PhotoURL:    requestBody.PhotoURL,
+	}
+
+	if err := db.Save(&user).Error; err != nil {
+		message := err.Error()
+		c.JSON(http.StatusInternalServerError, api.Error{
+			Message: &message,
+		})
+		return
+	}
 
 	c.JSON(http.StatusOK, user)
 }
