e7332ef387bf99197505827beaab4da2a84f850a
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-12 15:51:25 +0900

    fix follow status

--- model/follow_status.go
+++ model/follow_status.go
@@ -3,8 +3,10 @@ package model
 import "time"
 
 type FollowStatus struct {
-	From        User
-	To          User
+	FromUserId  string `gorm:"column:from_user_id"`
+	From        User   `gorm:"foreignkey:FromUserId"`
+	ToUserId    string `gorm:"column:to_user_id"`
+	To          User   `gorm:"foreignkey:ToUserId"`
 	RequestedAt time.Time
 	ApprovedAt  *time.Time
 }
