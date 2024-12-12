4f5d21cc02f5b20ca8e4845a1fd8ada2be47637a
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-12 17:58:30 +0900

    Feature/approve follow request (#19)
    * add approve follow request
    
    * add approve follow request

--- model/follow_status.go
+++ model/follow_status.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type FollowStatus struct {
-	FromId      string
+	FromId      string `gorm:"primaryKey"`
 	From        User
-	ToId        string
+	ToId        string `gorm:"primaryKey"`
 	To          User
 	RequestedAt time.Time
 	ApprovedAt  *time.Time
