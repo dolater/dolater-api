c6d6150f6f1b37a802d67e34eb5f2edd0efd24a0
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-12 15:55:12 +0900

    fix follow starus

--- model/follow_status.go
+++ model/follow_status.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type FollowStatus struct {
-	FromUserId  string `gorm:"column:from_user_id"`
-	From        User   `gorm:"foreignkey:FromUserId"`
-	ToUserId    string `gorm:"column:to_user_id"`
-	To          User   `gorm:"foreignkey:ToUserId"`
+	FromId      string
+	From        User
+	ToId        string
+	To          User
 	RequestedAt time.Time
 	ApprovedAt  *time.Time
 }
