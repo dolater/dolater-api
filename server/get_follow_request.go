feee191e513461d4465db671b3c2e07b6bf90f71
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-12 16:42:26 +0900

    fix get follow request

--- server/get_follow_request.go
+++ server/get_follow_request.go
@@ -49,5 +49,61 @@ func (s *Server) GetFollowRequest(c *gin.Context, uid string) {
 		})
 		return
 	}
-	c.JSON(http.StatusOK, followStatus)
+
+	fromUser := model.User{
+		Id: followStatus.FromId,
+	}
+	toUser := model.User{
+		Id: followStatus.ToId,
+	}
+	if err := db.First(&fromUser).Error; err != nil {
+		message := err.Error()
+		c.JSON(http.StatusNotFound, api.Error{
+			Message: &message,
+		})
+		return
+	}
+	if err := db.First(&toUser).Error; err != nil {
+		message := err.Error()
+		c.JSON(http.StatusNotFound, api.Error{
+			Message: &message,
+		})
+		return
+	}
+
+	response := api.FollowStatus{
+		From: api.User{
+			Id: fromUser.Id,
+			DisplayName: func() string {
+				if fromUser.DisplayName == nil {
+					return ""
+				}
+				return *fromUser.DisplayName
+			}(),
+			PhotoURL: func() string {
+				if fromUser.PhotoURL == nil {
+					return ""
+				}
+				return *fromUser.PhotoURL
+			}(),
+		},
+		To: api.User{
+			Id: toUser.Id,
+			DisplayName: func() string {
+				if toUser.DisplayName == nil {
+					return ""
+				}
+				return *toUser.DisplayName
+			}(),
+			PhotoURL: func() string {
+				if toUser.PhotoURL == nil {
+					return ""
+				}
+				return *toUser.PhotoURL
+			}(),
+		},
+		RequestedAt: followStatus.RequestedAt,
+		ApprovedAt:  followStatus.ApprovedAt,
+	}
+	c.JSON(http.StatusOK, response)
 }
