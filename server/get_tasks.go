ff879128518eac020e910a8d1d40ec5858eda5b1
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-12 11:35:48 +0900

    fix argument

--- server/get_tasks.go
+++ server/get_tasks.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (s *Server) GetTasks(c *gin.Context, pram api.GetTasksParams) {
+func (s *Server) GetTasks(c *gin.Context, poolId api.GetTasksParams) {
 	token := utility.GetToken(c)
 	if token == nil {
 		message := "Unauthorized"
