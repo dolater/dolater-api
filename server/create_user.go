7ea83e60041ade8eacdaeb23d8a3b7492da8b02c
Kanta Oikawa <k.oikawa@kantacky.com>
2024-12-14 07:04:14 +0900

    Fix issues on createUser

--- server/create_user.go
+++ server/create_user.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"slices"
 
 	"github.com/dolater/dolater-api/db"
 	api "github.com/dolater/dolater-api/generated"
 	"github.com/dolater/dolater-api/model"
 	"github.com/dolater/dolater-api/server/utility"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgconn"
 	"gorm.io/gorm"
 )
@@ -59,46 +61,6 @@ func (s *Server) CreateUser(c *gin.Context) {
 		PhotoURL:    &photoURL,
 	}
 
-	if err := db.Create(&user).Error; err != nil {
-		var pgErr *pgconn.PgError
-		if !errors.As(err, &pgErr) || pgErr.Code != "23505" {
-			message := err.Error()
-			c.AbortWithStatusJSON(http.StatusInternalServerError, api.Error{
-				Message: &message,
-			})
-			return
-		}
-	}
-
-	taskPools := []model.TaskPool{
-		{
-			OwnerId: user.Id,
-			Type:    string(api.Active),
-		},
-		{
-			OwnerId: user.Id,
-			Type:    string(api.Archived),
-		},
-		{
-			OwnerId: user.Id,
-			Type:    string(api.Pending),
-		},
-		{
-			OwnerId: user.Id,
-			Type:    string(api.Bin),
-		},
-	}
-
-	if err := db.Create(&taskPools).Error; err != nil {
-		if !errors.Is(err, gorm.ErrRecordNotFound) {
-			message := err.Error()
-			c.JSON(http.StatusInternalServerError, api.Error{
-				Message: &message,
-			})
-			return
-		}
-	}
-
 	response := api.User{
 		Id: user.Id,
 		DisplayName: func() string {
@@ -113,6 +75,68 @@ func (s *Server) CreateUser(c *gin.Context) {
 			}
 			return *user.PhotoURL
 		}(),
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		var pgErr *pgconn.PgError
+		if !errors.As(err, &pgErr) || pgErr.Code != "23505" {
+			message := err.Error()
+			c.AbortWithStatusJSON(http.StatusInternalServerError, api.Error{
+				Message: &message,
+			})
+			return
+		}
+	}
+
+	var taskPools []model.TaskPool
+
+	if err := db.
+		Find(&taskPools).
+		Where(&model.TaskPool{
+			OwnerId: token.UID,
+		}).
+		Error; err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			message := err.Error()
+			c.JSON(http.StatusInternalServerError, api.Error{
+				Message: &message,
+			})
+			return
+		}
+	}
+
+	taskPoolTypes := []api.TaskPoolType{
+		api.Active,
+		api.Archived,
+		api.Bin,
+		api.Pending,
+	}
+	existingTaskPoolTypes := []api.TaskPoolType{}
+
+	for _, pool := range taskPools {
+		if slices.Contains(taskPoolTypes, api.TaskPoolType(pool.Type)) && !slices.Contains(existingTaskPoolTypes, api.TaskPoolType(pool.Type)) {
+			existingTaskPoolTypes = append(existingTaskPoolTypes, api.TaskPoolType(pool.Type))
+		}
+	}
+
+	for _, poolType := range taskPoolTypes {
+		if !slices.Contains(existingTaskPoolTypes, poolType) {
+			taskPools = append(taskPools, model.TaskPool{
+				Id:      uuid.New(),
+				OwnerId: user.Id,
+				Type:    string(poolType),
+			})
+		}
+	}
+
+	if err := db.Save(&taskPools).Error; err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			message := err.Error()
+			c.JSON(http.StatusInternalServerError, api.Error{
+				Message: &message,
+			})
+			return
+		}
 	}
 
 	c.JSON(http.StatusCreated, response)
