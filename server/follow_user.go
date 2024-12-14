aa2b0f3486e861f7855fb5aa950c0ab58b6c74be
Masaya Osuga <136790650+masaya-osuga@users.noreply.github.com>
2024-12-14 15:27:16 +0900

    add followed notify (#46)
    * add followed notify
    
    * fix followed notify
    
    * add db.create notification
    
    * fix notification

--- server/follow_user.go
+++ server/follow_user.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 
+	"firebase.google.com/go/v4/messaging"
 	"github.com/dolater/dolater-api/db"
 	api "github.com/dolater/dolater-api/generated"
 	"github.com/dolater/dolater-api/model"
 	"github.com/dolater/dolater-api/server/utility"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
@@ -124,6 +126,88 @@ func (s *Server) FollowUser(c *gin.Context, uid string) {
 			}(),
 		},
 		Timestamp: followStatus.CreatedAt,
+	}
+
+	var fcmToken model.FCMToken
+
+	if err := db.
+		Where(&model.FCMToken{
+			UserId: followStatus.ToId,
+		}).
+		First(&fcmToken).
+		Error; err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			message := err.Error()
+			c.JSON(http.StatusInternalServerError, api.Error{
+				Message: &message,
+			})
+			return
+		}
+	}
+
+	notification := model.Notification{
+		Id:     uuid.New(),
+		UserId: response.To.Id,
+		Title:  "フォローバックしましょう!!",
+		Body: func() string {
+			if response.From.DisplayName == "" {
+				return "誰かがあなたをフォローしました"
+			}
+			return response.From.DisplayName + " さんがあなたをフォローしました"
+		}(),
+		URL: "https://dolater.kantacky.com/users/" + response.From.Id,
+	}
+
+	if err := db.Create(&notification).Error; err != nil {
+		message := err.Error()
+		c.JSON(http.StatusInternalServerError, api.Error{
+			Message: &message,
+		})
+		return
+	}
+
+	// 通知メッセージを作成
+	messages := []*messaging.Message{}
+	messages = append(messages, &messaging.Message{
+		Data: map[string]string{
+			"url": notification.URL,
+		},
+		Notification: &messaging.Notification{
+			Title: notification.Title,
+			Body:  notification.Body,
+		},
+		APNS: &messaging.APNSConfig{
+			Payload: &messaging.APNSPayload{
+				Aps: &messaging.Aps{
+					Sound: "default",
+				},
+			},
+		},
+		Token: fcmToken.RegistrationToken,
+	})
+
+	if len(messages) == 0 {
+		c.Status(http.StatusNoContent)
+		return
+	}
+
+	// 通知を送信
+	client, err := s.FirebaseApp.Messaging(c)
+	if err != nil {
+		message := err.Error()
+		c.AbortWithStatusJSON(http.StatusInternalServerError, api.Error{
+			Message: &message,
+		})
+		return
+	}
+
+	_, err = client.SendEach(c, messages)
+	if err != nil {
+		message := err.Error()
+		c.AbortWithStatusJSON(http.StatusInternalServerError, api.Error{
+			Message: &message,
+		})
+		return
 	}
 
 	c.JSON(http.StatusOK, response)
